754c112387e1198b6a0ae0a4db305228c309442f
LeAiMy <hantctk42@gmail.com>
2021-12-09 13:27:41 +0700

    Bổ sung file 😊😊😊

--- server/src/EStudy/Controller/Admin/AdminTopicController.php
+++ server/src/EStudy/Controller/Admin/AdminTopicController.php
@@ -2,12 +2,45 @@
 
 namespace EStudy\Controller\Admin;
 
+use EStudy\Model\Admin\TopicModel;
+use Ninja\NinjaException;
 use Ninja\NJBaseController\NJBaseController;
 
 class AdminTopicController extends NJBaseController
 {
+    private $topic_model;
+    public function __construct(TopicModel $topic_model)
+    {
+        $this->topic_model= $topic_model;
+    }
     public function index()
     {
         $this->view_handler->render('admin/topic/index.html.php');
+    }
+
+    public function create()
+    {
+        $this->view_handler->render('admin/topic/create.html.php');
+    }
+
+    public function store()
+    {
+        try {
+            $topic = $_POST;
+
+            $this->topic_model->create_new_topic($topic);
+    
+            $this->route_redirect('/admin/topics');
+        }
+        catch (NinjaException $e) {
+            // Xử lý lỗi
+
+            $this->view_handler->render('admin/topic/create.html.php', [
+                'title' => $_POST['title'] ?? null,
+                'description' => $_POST['description'] ?? null,
+                'error' => true,
+                'message' => $e->getMessage(),
+            ]);
+        }
     }
 }
