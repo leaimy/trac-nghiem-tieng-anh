e3cbf6d0f7cb162accf906b106c30bc0648d1621
LeAiMy <hantctk42@gmail.com>
2021-12-13 09:11:47 +0700

    Bổ sung file 🌼🌼🌼

--- server/src/EStudy/Controller/Admin/AdminTopicController.php
+++ server/src/EStudy/Controller/Admin/AdminTopicController.php
@@ -2,16 +2,21 @@
 
 namespace EStudy\Controller\Admin;
 
+use EStudy\Entity\Admin\TopicEntity;
+use EStudy\Model\Admin\MediaModel;
 use EStudy\Model\Admin\TopicModel;
+use Exception;
 use Ninja\NinjaException;
 use Ninja\NJBaseController\NJBaseController;
 
 class AdminTopicController extends NJBaseController
 {
     private $topic_model;
-    public function __construct(TopicModel $topic_model)
+    private $media_model;
+    public function __construct(TopicModel $topic_model, MediaModel $media_model)
     {
         $this->topic_model= $topic_model;
+        $this->media_model = $media_model;
     }
     public function index()
     {
@@ -32,12 +37,18 @@ class AdminTopicController extends NJBaseController
         try {
             $topic = $_POST;
 
+            if (isset($_FILES['file_upload'])) {
+               $new_media = $this->media_model->create_new_media($_FILES);
+               $media_id = $new_media->id;
+               $topic[TopicEntity::KEY_MEDIA_ID] = $media_id;
+            }
+
             $this->topic_model->create_new_topic($topic);
     
             $this->route_redirect('/admin/topics');
         }
         catch (NinjaException $e) {
-            // Xử lý lỗi
+            // Xử lý lỗi do mình tự ném ra
 
             $this->view_handler->render('admin/topic/create.html.php', [
                 'title' => $_POST['title'] ?? null,
@@ -46,5 +57,23 @@ class AdminTopicController extends NJBaseController
                 'message' => $e->getMessage(),
             ]);
         }
+        catch (Exception $e) {
+            // Xử lý lỗi từ hệ thống server / PHP
+        }
+    }
+
+    public function edit()
+    {
+        $id = $_GET['id'];
+        $topic = $this->topic_model->get_by_id($id);
+
+        $this->view_handler->render('admin/topic/edit.html.php', [
+            'topic' => $topic
+        ]);
+    }
+
+    public function update()
+    {
+
     }
 }
