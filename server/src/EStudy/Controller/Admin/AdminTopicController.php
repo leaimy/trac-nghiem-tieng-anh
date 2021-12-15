3f4a20656ad6e1813b1159bb185deeec12e71040
LeAiMy <hantctk42@gmail.com>
2021-12-15 08:55:36 +0700

    Bổ sung file topic controller 🍔🍔🍔

--- server/src/EStudy/Controller/Admin/AdminTopicController.php
+++ server/src/EStudy/Controller/Admin/AdminTopicController.php
@@ -15,7 +15,7 @@ class AdminTopicController extends NJBaseController
     private $media_model;
     public function __construct(TopicModel $topic_model, MediaModel $media_model)
     {
-        $this->topic_model= $topic_model;
+        $this->topic_model = $topic_model;
         $this->media_model = $media_model;
     }
     public function index()
@@ -24,7 +24,6 @@ class AdminTopicController extends NJBaseController
         $this->view_handler->render('admin/topic/index.html.php', [
             'topic_all' => $topic_all
         ]);
-        
     }
 
     public function create()
@@ -38,16 +37,15 @@ class AdminTopicController extends NJBaseController
             $topic = $_POST;
 
             if (isset($_FILES['file_upload'])) {
-               $new_media = $this->media_model->create_new_media($_FILES);
-               $media_id = $new_media->id;
-               $topic[TopicEntity::KEY_MEDIA_ID] = $media_id;
+                $new_media = $this->media_model->create_new_media($_FILES);
+                $media_id = $new_media->id;
+                $topic[TopicEntity::KEY_MEDIA_ID] = $media_id;
             }
 
             $this->topic_model->create_new_topic($topic);
-    
+
             $this->route_redirect('/admin/topics');
-        }
-        catch (NinjaException $e) {
+        } catch (NinjaException $e) {
             // Xử lý lỗi do mình tự ném ra
 
             $this->view_handler->render('admin/topic/create.html.php', [
@@ -56,8 +54,7 @@ class AdminTopicController extends NJBaseController
                 'error' => true,
                 'message' => $e->getMessage(),
             ]);
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             // Xử lý lỗi từ hệ thống server / PHP
         }
     }
@@ -74,6 +71,42 @@ class AdminTopicController extends NJBaseController
 
     public function update()
     {
+        try {
+            $topic = $_POST;
+            $id = $_POST['id'];
 
+            if (!empty($_FILES['file_upload']['name'])) {
+                // Người dùng cập nhật ảnh mới
+
+                $new_media = $this->media_model->create_new_media($_FILES);
+                $media_id = $new_media->id;
+                $topic[TopicEntity::KEY_MEDIA_ID] = $media_id;
+                $this->topic_model->update_topic($id, $topic);
+            } else {
+                // Người dùng chỉ cập nhật thông tin cơ bản
+                $this->topic_model->update_topic($id, $topic);
+            }
+
+
+            $this->route_redirect('/admin/topics');
+        } catch (NinjaException $e) {
+            // Xử lý lỗi do mình tự ném ra
+
+            $this->view_handler->render('admin/topic/edit.html.php', [
+                'title' => $_POST['title'] ?? null,
+                'description' => $_POST['description'] ?? null,
+                'error' => true,
+                'message' => $e->getMessage(),
+            ]);
+        } catch (Exception $e) {
+            // Xử lý lỗi từ hệ thống server / PHP
+        }
+    }
+
+    public function delete()
+    {
+        $id = $_GET['id'];
+         $this->topic_model->delete_topic($id);
+         $this->route_redirect('/admin/topics');
     }
 }
