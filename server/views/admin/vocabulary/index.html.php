835283442f677240218997508068b5adc74369d9
LeAiMy <hantctk42@gmail.com>
2021-12-15 09:29:29 +0700

    Test branch

--- server/views/admin/vocabulary/index.html.php
+++ server/views/admin/vocabulary/index.html.php
@@ -5,7 +5,7 @@
 {% block content %}
 
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Quản lý từ vựng</h1>
+    <h1 class="h2">Quản lý từ vựng 123</h1>
 </div>
 
 <div class="vh-100"></div>
