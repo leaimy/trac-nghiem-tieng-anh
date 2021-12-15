822ed67c4aab08fcbd842703aab27adc4e2c414b
LeAiMy <hantctk42@gmail.com>
2021-12-15 08:56:06 +0700

    Bổ sung file media controller 🍘🍘🍘

--- server/src/EStudy/Controller/Admin/AdminMediaController.php
+++ server/src/EStudy/Controller/Admin/AdminMediaController.php
@@ -2,12 +2,22 @@
 
 namespace EStudy\Controller\Admin;
 
+use EStudy\Model\Admin\MediaModel;
 use Ninja\NJBaseController\NJBaseController;
 
 class AdminMediaController extends NJBaseController
 {
+    private $media_model;
+    public function __construct(MediaModel $media_model)
+    {
+        $this->media_model = $media_model;
+    }
+
     public function index()
     {
-        $this->view_handler->render('admin/media/index.html.php');
+        $media_all = $this->media_model->get_all_media();
+        $this->view_handler->render('admin/media/index.html.php', [
+            'media_all' => $media_all
+        ]);
     }
 }
