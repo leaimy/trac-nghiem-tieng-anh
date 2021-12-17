cb09f055388b58b98c6015e3778c71397575bff8
DalatCoder <hieuntctk42@gmail.com>
2021-12-17 22:46:30 +0700

    Fix format

--- server/src/Ninja/DatabaseTable.php
+++ server/src/Ninja/DatabaseTable.php
@@ -219,7 +219,7 @@ class DatabaseTable
     {
         foreach ($fields as $key => $value) {
             if ($value instanceof \DateTime) {
-                $fields[$key] = $value->format('Y-m-d');
+                $fields[$key] = $value->format('Y-m-d H:i:s');
             }
         }
 
