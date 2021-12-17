c4bd3ea6880729e2ed43f11a5d43a8641729fa94
DalatCoder <hieuntctk42@gmail.com>
2021-12-17 07:46:23 +0700

    Add import for Lac Viet

--- server/src/EStudy/Controller/Admin/AdminImportController.php
+++ server/src/EStudy/Controller/Admin/AdminImportController.php
@@ -4,6 +4,7 @@ namespace EStudy\Controller\Admin;
 
 use EStudy\Model\Import\QuestionBank\ICT;
 use EStudy\Model\Import\QuestionBank\Quizlet;
+use EStudy\Model\Import\Vocabulary\LacViet;
 use Ninja\NJBaseController\NJBaseController;
 
 class AdminImportController extends NJBaseController
@@ -24,6 +25,14 @@ class AdminImportController extends NJBaseController
     public function import_quizlet()
     {
         $instance = new Quizlet();
+        $instance->populate();
+
+        $this->route_redirect('/admin/import-sample-data');
+    }
+    
+    public function import_lacviet_vocabulary()
+    {
+        $instance = new LacViet();
         $instance->populate();
 
         $this->route_redirect('/admin/import-sample-data');
